63f193f808349ef39d7cba40c3b840c649cf7eb0
ocelik66il08e <usaonurcelik@gmail.com>
2020-08-02 12:47:18 -0400

    day37 class

--- src/day37_ArrayList/BulkOperations_Practice.java
+++ src/day37_ArrayList/BulkOperations_Practice.java
@@ -24,5 +24,32 @@ public class BulkOperations_Practice {
         boolean r2 = students.containsAll(Arrays.asList("Frank", "Henry", "Ali"));
         System.out.println(r2);
 
+        System.out.println("===================================================");
+
+        ArrayList<String> namesOfGroup = new ArrayList<>();
+
+        namesOfGroup.addAll(Arrays.asList("Obama", "Trump", "Washington", "Clinton", "Carter", "Ford"));
+        System.out.println(namesOfGroup);
+        System.out.println("==========");
+        boolean names = namesOfGroup.containsAll(Arrays.asList("Kennedy", "Bush", "Nixon", "Eisenhower"));
+        System.out.println(names);
+        System.out.println("=============================");
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.addAll(Arrays.asList(1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 6, 6, 6, 6, 7, 7, 7, 8, 8, 9, 10, 11, 12, 13));
+        System.out.println(nums);
+        System.out.println("========");
+        nums.removeAll(Arrays.asList(1, 2, 5, 10, 11));
+        System.out.println(nums);
+        System.out.println("======================================================================");
+
+        ArrayList<Integer> nums2 = new ArrayList<>();
+        nums2.addAll(Arrays.asList(10, 10, 10, 10, 20, 20, 20, 30, 40, 60, 60, 70, 70, 80, 80, 90, 10, 11, 12, 13));
+        System.out.println(nums2);
+        System.out.println("========");
+
+        nums2.retainAll(Arrays.asList(30, 11, 12, 13));
+        System.out.println(nums2);
+
+
     }
 }
---
+++ src/day37_ArrayList/Task_BulkOperations.java
@@ -0,0 +1,24 @@
+package day37_ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task_BulkOperations {
+    public static void main(String[] args) {
+
+
+        ArrayList<String> employeeList = new ArrayList<>();
+        employeeList.addAll(Arrays.asList("Ali", "Veli", "Ahmed", "Mehmed", "Turan", "Ahmed", "Suat"));
+
+        // Remove employee name is Ahmed.
+        employeeList.removeAll(Arrays.asList("Ahmed"));
+        System.out.println("Updated Employee List : " + employeeList);
+        System.out.println("=====================");
+
+        // Remove all employee on list without 'Ali'.
+        employeeList.retainAll(Arrays.asList("Ali"));
+        System.out.println(employeeList);
+
+
+    }
+}
